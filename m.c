70743011a53d0ce42cfda54c39346dadb41a72ea
nseEMgroup <aoc1073@naver.com>
2024-12-02 22:28:50 +0900

    modified:   m.c 	modified:   m.out

--- m.c
+++ m.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include <sys/sysinfo.h>
+#include <stdlib.h>
 
 int main(){
-	printf("memory test\n");
+	FILE *fp;
+	struct sysinfo meminfo;
+	fp = fopen("memoryreport.txt", "w");
+	sysinfo(&meminfo);
+	fprintf(fp, "총 메모리 용량: %.3fGB\n", (float)meminfo.totalram/(1024*1024*1024));
+	fprintf(fp, "사용자 사용 가능 메모리 용량: %.3fGB\n", (float)meminfo.totalhigh/(1024*1024*1024));
+	fprintf(fp, "총 스왑 메모리 용량: %.3fGB\n", (float)meminfo.totalswap/(1024*1024*1024));
+	fclose(fp);
+	printf("메모리 보고서 작성 완료\n");
+	exit(0);
 }
